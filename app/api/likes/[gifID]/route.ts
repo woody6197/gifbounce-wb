1b85f64fa66bd4831d828fa0aec1d389658d7574
Dan Edwards <igneosaur@gmail.com>
2023-10-06 22:02:16 +0100

    Use env var for userID

--- app/api/likes/[gifID]/route.ts
+++ app/api/likes/[gifID]/route.ts
@@ -1,18 +1,13 @@
 import prisma from "@/lib/prisma";
-import { cookies } from "next/headers";
 
-export type Params = Record<
-  "params",
-  {
-    gifID: string;
-  }
->;
+export type Params = Record<"params", { gifID: string }>;
 
 export async function POST(_: Request, { params }: Params) {
-  const cookieStore = cookies();
-  const user_id = cookieStore.get("userID")!.value;
+  if (!process.env.USER_ID) {
+    throw new Error("`USER_ID` env variable is missing.");
+  }
   const data = await prisma.likes.create({
-    data: { user_id, gif_id: params.gifID },
+    data: { user_id: process.env.USER_ID, gif_id: params.gifID },
   });
   return Response.json({ data });
 }
