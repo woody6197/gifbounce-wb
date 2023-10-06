52d0cef6d0c6440a908dc5e53f76999b317a91e5
Dan Edwards <igneosaur@gmail.com>
2023-10-06 17:26:49 +0100

    Remove memeFeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
-import { Suspense } from "react";
-import Loading from "./loading";
-import MemeFeed from "./memeFeed";
+import { getTrendingData } from "@/lib/giphy";
+import Image from "next/image";
+import LikeButton from "./likeButton";
 
-// const MemeFeed = lazy(() => import("./memeFeed"));
-
-export default function Home() {
+export default async function Home() {
+  const data = await getTrendingData({ limit: 10, rating: "g" });
   return (
     <main className="main bg-zinc-800/30 h-screen overflow-auto snap-mandatory snap-y">
-      <Suspense fallback={<Loading />}>
-        <MemeFeed options={{ limit: 10, rating: "g" }} />
-      </Suspense>
+      {data.map(({ id, images, title }) => {
+        const gif_id = String(id);
+        return (
+          <section
+            id={gif_id}
+            key={gif_id}
+            className="h-screen snap-center relative p-5"
+          >
+            <Image
+              className="w-full h-screen object-cover absolute top-0 left-0 z-[-1]"
+              src={images.original.webp}
+              alt={title}
+              width={512}
+              height={512}
+              priority
+            />
+            <LikeButton gifID={gif_id} />
+          </section>
+        );
+      })}
     </main>
   );
 }
